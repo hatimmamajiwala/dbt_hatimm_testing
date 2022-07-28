241e4a69352d109b0fa5ace23b2e3ee73e787a2e
Hatim Mama <hatim786@gmail.com>
2022-07-28 12:18:31 +0000

    Commit

--- macros/grant_select.sql
+++ macros/grant_select.sql
@@ -1,12 +1,13 @@
 {% macro grant_select(schema=target.schema, usr_role='DATAENGG') %}
 
    {% set query %}
+       GRANT USAGE ON DATABASE ANALYTICS to role {{ usr_role }}; 
        grant usage on schema {{ schema }} to role {{ usr_role }};
        grant select on all tables in schema {{ schema }} to role {{ usr_role }};
        grant select on all views in schema {{ schema }} to role {{ usr_role }};
    {% endset %}
    
-   {{ log('Granting all privileges to of select and view to schema ' ~ schema ~ ' for the role ' ~ usr_role, info=true )}}
+   {{ log('Granting all privileges to of select and view to schema ' ~ schema ~ ' for the role ' ~ usr_role ~ ' for the database ANALYTICS', info=true )}}
    
    {% do run_query(query) %}
 
